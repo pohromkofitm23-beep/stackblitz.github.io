7d77f67022dc615785b5727a4d40f3889da5816e
pohromkofitm23-beep <pohromko.fitm23@kubg.edu.ua>
2026-05-25 22:50:00 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://mod.kubg.edu.ua',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '/cgi-bin/timetable.cgi')
+      }
+    }
+  }
 })
